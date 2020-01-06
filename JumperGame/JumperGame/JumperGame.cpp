ef995b30d16fd2a54eaa0ff3ca7b9d78867d03a6
Zi Tan <zitan39@gmail.com>
2020-01-05 18:03:34 -0800

    Added Collision Detection

--- JumperGame/JumperGame/JumperGame.cpp
+++ JumperGame/JumperGame/JumperGame.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include "pch.h"
 using namespace std;
@@ -11,7 +11,7 @@ class JumperGame : public olcConsoleGameEngine
 public:
 	JumperGame()
 	{
-		m_sAppName = L"Flappy Bird";
+		m_sAppName = L"Jumper";
 	}
 
 private:
@@ -26,6 +26,12 @@ private:
 
 	float levelPosition = 0.0f;
 
+	int jumpCount = 0;
+
+	bool collision = false;
+	bool newGame = false;
+
+
 protected:
 	// Called by olcConsoleGameEngine
 	virtual bool OnUserCreate()
@@ -39,67 +45,98 @@ protected:
 	// Called by olcConsoleGameEngine
 	virtual bool OnUserUpdate(float fElapsedTime)
 	{
-		if (m_keys[VK_SPACE].bPressed && jumperPosition == 40) {
-			//Stop falling, fly up
+
+		//New Game - initialize values
+		if (newGame) {
+			collision = false;
+			newGame = false;
+			listSection = { 0, 0, 0, 0 };
+			jumperPosition = 0.0f;
+			jumperVelocity = 0.0f;
 			jumperAcceleration = 0.0f;
-			jumperVelocity = -gravity / 4.0f;
-		}
-		else
-			//Increase effect of gravity
-			jumperAcceleration += gravity * fElapsedTime;
-
-		if (jumperAcceleration >= gravity)
-			jumperAcceleration = gravity;
-
-		jumperVelocity += jumperAcceleration * fElapsedTime;
-		jumperPosition += jumperVelocity * fElapsedTime;
-
-		//Floor is at position 40
-		if (jumperPosition > 40) {
-			jumperPosition = 40;
 		}
 
-		//14 is speed level
-		levelPosition += 14.0f * fElapsedTime;
-
-		if(levelPosition > sectionWidth) {
-			levelPosition -= sectionWidth;
+		if (collision) {
+			DrawString(1, 1, L"GAME OVER");
+			DrawString(1, 2, L"PRESS SPACE TO RESET");
+			//Space key resets game
+			if (m_keys[VK_SPACE].bPressed) {
+				newGame = true;
+			}
 		}
 
-		//Clear Screen - remove image of bird old position
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
+		else {
 
-		//Keep Bird at same X position
-		int JumperPositionX = (int)(ScreenWidth() / 3.0f);
+			if (m_keys[VK_SPACE].bPressed && jumpCount < 2 && jumperVelocity >= gravity / 5.0f) {
+				//Stop falling, fly up
+				jumperAcceleration = 0.0f;
+				jumperVelocity = -gravity * 2 / 4.0f;
+				jumpCount++;
+			}
+			else
+				//Increase effect of gravity
+				jumperAcceleration += gravity * fElapsedTime;
 
-		//Draw Bird
-		if (jumperVelocity > 0)
-		{
-			/*
-			DrawString(JumperPositionX, jumperPosition + 0, L"\\\\\\");
-			DrawString(JumperPositionX, jumperPosition + 1, L"<\\\\\\=Q");
-			*/
-			DrawString(JumperPositionX, jumperPosition + 0, L"         /  \~~~/  \"");
-			DrawString(JumperPositionX, jumperPosition + 1, L"   ,----(     ..    )");
-			DrawString(JumperPositionX, jumperPosition + 2, L"  /      \__     __/");
-			DrawString(JumperPositionX, jumperPosition + 3, L" /|         (\  |(");
-			DrawString(JumperPositionX, jumperPosition + 4, L"^ \   /___\  /\ |");
-			DrawString(JumperPositionX, jumperPosition + 5, L"  |__|   |__|-");
+			if (jumperAcceleration >= gravity)
+				jumperAcceleration = gravity;
+
+			jumperVelocity += jumperAcceleration * fElapsedTime;
+			jumperPosition += jumperVelocity * fElapsedTime;
+			//14 is speed level
+			levelPosition += 14.0f * fElapsedTime;
+
+			//Roof is at position 1
+			if (jumperPosition < 1) {
+				jumperPosition = 1;
+			}
+			//Floor is at position 47
+			else if (jumperPosition > 47) {
+				jumperPosition = 47;
+			}
+
+			//Reset Double Jump when Jumper touches ground
+			if (jumperPosition == 47) {
+				jumpCount = 0;
+			}
+
+			//Get Random obstacles
+			if (levelPosition > sectionWidth) {
+				levelPosition -= sectionWidth;
+				listSection.pop_front();
+				int i = rand() % (ScreenHeight() - 20);
+				if (i <= 10) i = 0;
+				listSection.push_back(i);
+			}
+
+			//Clear Screen - remove image of bird old position
+			Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
+
+			int nSection = 0;
+			for (auto s : listSection) {
+				if (s != 0) {
+					Fill(nSection * sectionWidth + 10 - levelPosition, ScreenHeight() - s, nSection * sectionWidth + 15 - levelPosition, ScreenHeight(), PIXEL_SOLID, FG_DARK_CYAN);
+				}
+				nSection++;
+			}
+			//Keep Jumper at same X position
+			int JumperPositionX = (int)(ScreenWidth() / 3.0f);
+
+			//Check if Jumper collided with obstacle
+			collision = m_bufScreen[(int)(jumperPosition + 0) * ScreenWidth() + JumperPositionX].Char.UnicodeChar != L' ' ||
+				m_bufScreen[(int)(jumperPosition + 0) * ScreenWidth() + JumperPositionX + 6].Char.UnicodeChar != L' ';
+
+			//Draw the Jumper
+			if (jumperVelocity > 0 && jumperPosition != 47)
+			{
+				//Jumper image when falling
+				DrawString(JumperPositionX, jumperPosition, L"(xʖx)");
+			}
+			else
+			{
+				//Jumper image when jumping
+				DrawString(JumperPositionX, jumperPosition, L"(°ʖ°)");
+			}
 		}
-		else
-		{
-			/*
-			DrawString(JumperPositionX, jumperPosition + 0, L"<///=Q");
-			DrawString(JumperPositionX, jumperPosition + 1, L"///");
-			*/
-			DrawString(JumperPositionX, jumperPosition + 0, L"         /  \~~~/  \"");
-			DrawString(JumperPositionX, jumperPosition + 1, L"   ,----(     ..    )");
-			DrawString(JumperPositionX, jumperPosition + 2, L"  /      \__     __/");
-			DrawString(JumperPositionX, jumperPosition + 3, L" /|         (\  |(");
-			DrawString(JumperPositionX, jumperPosition + 4, L"^ \   /___\  /\ |");
-			DrawString(JumperPositionX, jumperPosition + 5, L"  |__|   |__|-");
-		}
-
 		return true;
 	}
 };
@@ -111,6 +148,5 @@ int main()
 	JumperGame game;
 	game.ConstructConsole(80, 48, 16, 16);
 	game.Start();
-
 	return 0;
 }
